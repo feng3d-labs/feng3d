8cc5cb584050bbbd7421be764568199a068d4c29
feng <908087098@qq.com>
2016-04-15 23:56:58 +0800

    重新 构建feng3d

---
+++ feng3d/src/me/feng3d/events/base/EventDispatcher.ts
@@ -0,0 +1,109 @@
+module feng3d {
+
+	/**
+	 * 为了实现非flash原生显示列表的冒泡事件，自定义事件适配器
+	 * @author feng 2016-3-22
+	 */
+    export class EventDispatcher implements IEventDispatcher {
+        name: string;
+        _target: IEventDispatcher;
+        _eventListeners: any;
+
+        /** 冒泡属性名称为“parent” */
+        public static BUBBLE_PROPERTY: string = "parent";
+
+		/**
+		 * 构建事件适配器
+		 * @param target		事件适配主体
+		 */
+        constructor(target: IEventDispatcher = null) {
+            this._target = target;
+            if (this._target == null)
+                this._target = this;
+            this._eventListeners = {};
+        }
+
+        public addEventListener(type: string,thisObject:IEventDispatcher, listener: (event:Event)=>any, priority: number = 0, useWeakReference: boolean = false): void {
+            if (listener == null)
+                return;
+
+            var listeners: Function[] = this._eventListeners[type];
+            if (listeners == null)
+                listeners = this._eventListeners[type] = [];
+            var index = listeners.indexOf(listener);
+            if (index == -1) {
+                listeners.push(listener);
+            }
+        }
+
+        public removeEventListener(type: string, listener: (event:Event)=>any): void {
+            if (this._eventListeners) {
+                var listeners: Function[] = this._eventListeners[type];
+                var index: number = listeners.indexOf(listener);
+                listeners.splice(index, 1);
+            }
+        }
+
+        public removeEventListeners(type: string = null): void {
+            if (type && this._eventListeners)
+                delete this._eventListeners[type];
+            else
+                this._eventListeners = {};
+        }
+
+		/**
+		 * @inheritDoc
+		 */
+        public dispatchEvent(event: Event): boolean {
+            //停止事件流
+            if (!event || event.stopsPropagation)
+                return false;
+
+            //设置目标
+            if (!event.target)
+                event.setTarget(this._target);
+
+            //处理当前事件(目标阶段)
+            var listeners: Function[] = this._eventListeners[event.type];
+            if (!event.stopsImmediatePropagation) {
+                listeners.forEach(listener => {
+                    //设置当前目标
+                    event.setCurrentTarget(this._target);
+                    listener(event);
+                });
+            }
+
+            //事件冒泡(冒泡阶段)
+            if (event.bubbles && this.parentDispatcher) {
+                this.parentDispatcher.dispatchEvent(event);
+            }
+
+            return event.stopsPropagation;
+        }
+
+        public hasEventListener(type: string): boolean {
+            var listeners: any = this._eventListeners ? this._eventListeners[type] : null;
+            for (var key in listeners) {
+                return true;
+            }
+            return false;
+        }
+
+		/**
+		 * 该功能暂未实现
+		 * @param type
+		 * @return
+		 */
+        public willTrigger(type: string): boolean {
+            // TODO Auto Generated method stub
+            return false;
+        }
+
+		/**
+		 * 父事件适配器
+		 */
+        private get parentDispatcher(): IEventDispatcher {
+            return this._target[EventDispatcher.BUBBLE_PROPERTY];
+        }
+    }
+}
---
+++ feng3d/src/me/feng3d/events/base/IEventDispatcher.ts
@@ -0,0 +1,34 @@
+module feng3d {
+
+    /**
+     * IEventDispatcher 接口定义用于添加或删除事件侦听器的方法，检查是否已注册特定类型的事件侦听器，并调度事件。
+     * @author feng 2016-3-22
+     */
+    export interface IEventDispatcher {
+
+        /**
+         * 使用 EventDispatcher 对象注册事件侦听器对象，以使侦听器能够接收事件通知。
+         */
+        addEventListener(type: string, thisObject: any, listener: (event: Event) => any, priority?: number): void;
+
+        /**
+         * 将事件调度到事件流中。
+         */
+        dispatchEvent(event: Event): boolean;
+
+        /**
+         * 检查 EventDispatcher 对象是否为特定事件类型注册了任何侦听器。
+         */
+        hasEventListener(type: string): boolean;
+
+        /**
+         * 从 EventDispatcher 对象中删除侦听器。
+         */
+        removeEventListener(type: string, thisObject: any, listener: (event: Event) => any): void;
+
+        /**
+         * 检查是否用此 EventDispatcher 对象或其任何祖代为指定事件类型注册了事件侦听器。
+         */
+        willTrigger(type: string): boolean;
+    }
+}
