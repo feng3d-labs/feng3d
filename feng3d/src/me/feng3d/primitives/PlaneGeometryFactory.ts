e371f1928552e1f271a0329947ccd21e7c3913e9
feng <908087098@qq.com>
2016-04-30 23:05:48 +0800

    调整 生成 平面方法

--- feng3d/src/me/feng3d/primitives/PlaneGeometryFactory.ts
+++ feng3d/src/me/feng3d/primitives/PlaneGeometryFactory.ts
@@ -1,35 +1,63 @@
 module me.feng3d.primitives {
 
     /**
-     * 创建
+     * 创建平面几何体
+     * @param width 宽度
+     * @param height 高度
+     * @param segmentsW 横向分割数
+     * @param segmentsH 纵向分割数
+     * @param yUp 正面朝向 true:Y+ false:Z+
      */
-    export function creatPlane(plane: PlanePrimitive): Geometry {
+    export function createPlane(width = 100, height = 100, segmentsW = 1, segmentsH = 1, yUp = true, elements = [GLAttribute.position, GLAttribute.normal, GLAttribute.tangent]): Geometry {
 
-        var target = new Geometry();
+        var geometry = new Geometry();
+
+        elements.forEach(element => {
+            switch (element) {
+                case GLAttribute.position:
+                    var vertexPositionData = buildPosition(width, height, segmentsW, segmentsH, yUp);
+                    geometry.setVAData(GLAttribute.position, vertexPositionData, 3);
+                    break;
+                case GLAttribute.normal:
+                    var vertexNormalData = buildNormal(segmentsW, segmentsH, yUp);
+                    geometry.setVAData(GLAttribute.normal, vertexNormalData, 3)
+                    break;
+                case GLAttribute.tangent:
+                    var vertexTangentData = buildTangent(segmentsW, segmentsH, yUp);
+                    geometry.setVAData(GLAttribute.tangent, vertexTangentData, 3)
+                    break;
+                default:
+                    throw (`不支持为平面创建顶点属性 ${element}`);
+            }
+        });
+
+        var indices = buildIndices(segmentsW, segmentsH, yUp);
+        geometry.indices = indices;
+
+        return geometry;
+    }
+
+    /**
+     * 构建顶点坐标
+     * @param width 宽度
+     * @param height 高度
+     * @param segmentsW 横向分割数
+     * @param segmentsH 纵向分割数
+     * @param yUp 正面朝向 true:Y+ false:Z+
+     */
+    function buildPosition(width = 100, height = 100, segmentsW = 1, segmentsH = 1, yUp = true): number[] {
 
         var vertexPositionData: number[] = [];
-        var vertexNormalData: number[] = [];
-        var vertexTangentData: number[] = [];
-        var indices: number[] = [];
-        var tw: number = this._segmentsW + 1;
-        var vertexPositionStride: number = 3;
-        var vertexNormalStride: number = 3;
-        var vertexTangentStride: number = 3;
-
-        var numIndices = 0;
-        var positionIndex: number = 0;
-        var normalIndex: number = 0;
-        var tangentIndex: number = 0;
         var x: number, y: number;
-        var base: number;
-        for (var yi: number = 0; yi <= this._segmentsH; ++yi) {
-            for (var xi: number = 0; xi <= this._segmentsW; ++xi) {
-                x = (xi / this._segmentsW - .5) * this._width;
-                y = (yi / this._segmentsH - .5) * this._height;
+        var positionIndex: number = 0;
+        for (var yi: number = 0; yi <= segmentsH; ++yi) {
+            for (var xi: number = 0; xi <= segmentsW; ++xi) {
+                x = (xi / segmentsW - .5) * width;
+                y = (yi / segmentsH - .5) * height;
 
                 //设置坐标数据
                 vertexPositionData[positionIndex++] = x;
-                if (this._yUp) {
+                if (yUp) {
                     vertexPositionData[positionIndex++] = 0;
                     vertexPositionData[positionIndex++] = y;
                 }
@@ -37,10 +65,28 @@ module me.feng3d.primitives {
                     vertexPositionData[positionIndex++] = y;
                     vertexPositionData[positionIndex++] = 0;
                 }
+            }
+        }
+        return vertexPositionData;
+    }
+
+    /**
+     * 构建顶点法线
+     * @param segmentsW 横向分割数
+     * @param segmentsH 纵向分割数
+     * @param yUp 正面朝向 true:Y+ false:Z+
+     */
+    function buildNormal(segmentsW = 1, segmentsH = 1, yUp = true): number[] {
+
+        var vertexNormalData: number[] = [];
+
+        var normalIndex: number = 0;
+        for (var yi: number = 0; yi <= segmentsH; ++yi) {
+            for (var xi: number = 0; xi <= segmentsW; ++xi) {
 
                 //设置法线数据
                 vertexNormalData[normalIndex++] = 0;
-                if (this._yUp) {
+                if (yUp) {
                     vertexNormalData[normalIndex++] = 1;
                     vertexNormalData[normalIndex++] = 0;
                 }
@@ -48,58 +94,62 @@ module me.feng3d.primitives {
                     vertexNormalData[normalIndex++] = 0;
                     vertexNormalData[normalIndex++] = -1;
                 }
+            }
+        }
+        return vertexNormalData;
+    }
+
+    /**
+     * 构建顶点切线
+     * @param segmentsW 横向分割数
+     * @param segmentsH 纵向分割数
+     * @param yUp 正面朝向 true:Y+ false:Z+
+     */
+    function buildTangent(segmentsW = 1, segmentsH = 1, yUp = true): number[] {
+
+        var vertexTangentData: number[] = [];
+        var tangentIndex: number = 0;
+        for (var yi: number = 0; yi <= segmentsH; ++yi) {
+            for (var xi: number = 0; xi <= segmentsW; ++xi) {
 
                 vertexTangentData[tangentIndex++] = 1;
                 vertexTangentData[tangentIndex++] = 0;
                 vertexTangentData[tangentIndex++] = 0;
+            }
+        }
+        return vertexTangentData;
+    }
 
-                //复制反面数据
-                if (this._doubleSided) {
-                    for (var i: number = 0; i < 3; ++i) {
-                        vertexPositionData[positionIndex] = vertexPositionData[positionIndex - vertexPositionStride];
-                        ++positionIndex;
-                    }
-                    for (i = 0; i < 3; ++i) {
-                        vertexPositionData[normalIndex] = -vertexPositionData[normalIndex - vertexNormalStride];
-                        ++normalIndex;
-                    }
-                    for (i = 0; i < 3; ++i) {
-                        vertexTangentData[tangentIndex] = -vertexTangentData[tangentIndex - vertexTangentStride];
-                        ++tangentIndex;
-                    }
-                }
+    /**
+     * 构建顶点索引
+     * @param segmentsW 横向分割数
+     * @param segmentsH 纵向分割数
+     * @param yUp 正面朝向 true:Y+ false:Z+
+     */
+    function buildIndices(segmentsW = 1, segmentsH = 1, yUp = true): number[] {
+
+        var indices: number[] = [];
+        var tw: number = segmentsW + 1;
+
+        var numIndices = 0;
+        var base: number;
+        for (var yi: number = 0; yi <= segmentsH; ++yi) {
+            for (var xi: number = 0; xi <= segmentsW; ++xi) {
 
                 //生成索引数据
-                if (xi != this._segmentsW && yi != this._segmentsH) {
+                if (xi != segmentsW && yi != segmentsH) {
                     base = xi + yi * tw;
-                    var mult: number = this._doubleSided ? 2 : 1;
 
-                    indices[numIndices++] = base * mult;
-                    indices[numIndices++] = (base + tw) * mult;
-                    indices[numIndices++] = (base + tw + 1) * mult;
-                    indices[numIndices++] = base * mult;
-                    indices[numIndices++] = (base + tw + 1) * mult;
-                    indices[numIndices++] = (base + 1) * mult;
-
-                    //设置反面索引数据
-                    if (this._doubleSided) {
-                        indices[numIndices++] = (base + tw + 1) * mult + 1;
-                        indices[numIndices++] = (base + tw) * mult + 1;
-                        indices[numIndices++] = base * mult + 1;
-                        indices[numIndices++] = (base + 1) * mult + 1;
-                        indices[numIndices++] = (base + tw + 1) * mult + 1;
-                        indices[numIndices++] = base * mult + 1;
-                    }
+                    indices[numIndices++] = base;
+                    indices[numIndices++] = base + tw;
+                    indices[numIndices++] = base + tw + 1;
+                    indices[numIndices++] = base;
+                    indices[numIndices++] = base + tw + 1;
+                    indices[numIndices++] = base + 1;
                 }
             }
         }
 
-        target.setVAData(GLAttribute.position, vertexPositionData, vertexPositionStride)
-        target.setVAData(GLAttribute.normal, vertexNormalData, vertexNormalStride)
-        target.setVAData(GLAttribute.tangent, vertexTangentData, vertexTangentStride)
-
-        target.indices = indices;
-
-        return target;
+        return indices;
     }
 }
