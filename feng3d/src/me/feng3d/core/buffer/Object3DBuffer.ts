bd31d554079cdd12edba024ced3a2bdedcc80918
feng <908087098@qq.com>
2016-05-26 10:13:51 +0800

    制作 顶点数据缓冲

---
+++ feng3d/src/me/feng3d/core/buffer/Object3DBuffer.ts
@@ -0,0 +1,12 @@
+module me.feng3d {
+
+    /**
+     * 3D对象缓冲
+     */
+    export class Object3DBuffer {
+
+        squareVerticesBuffer: WebGLBuffer;
+        indexBuffer: WebGLBuffer;
+        count: number;
+    }
+}
