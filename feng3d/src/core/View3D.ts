2cbb69ac95829bed09bc5ce29188a2120a9d41ac
feng <908087098@qq.com>
2016-10-11 09:12:11 +0800

    整理 代码

--- feng3d/src/core/View3D.ts
+++ feng3d/src/core/View3D.ts
@@ -6,7 +6,7 @@ module feng3d {
      */
     export class View3D {
 
-        private gl: WebGLRenderingContext;
+        private context3d: WebGLRenderingContext;
 
         private _camera: Camera3D;
 
@@ -36,12 +36,12 @@ module feng3d {
             assert(canvas instanceof HTMLCanvasElement, `canvas参数必须为 HTMLCanvasElement 类型！`);
             this.canvas = canvas;
 
-            this.gl = this.canvas.getContext("experimental-webgl");
-            this.gl || alert("Unable to initialize WebGL. Your browser may not support it.");
-           
+            this.context3d = this.canvas.getContext("experimental-webgl");
+            this.context3d || alert("Unable to initialize WebGL. Your browser may not support it.");
+
             this.scene = scene || new Scene3D();
-            this._camera = camera || new Camera3D();
-            this.renderer = new Renderer(this.gl, this.scene, this._camera);
+            this.camera = camera || new Camera3D();
+            this.renderer = new Renderer(this.context3d, this.scene, this._camera);
 
             setInterval(this.drawScene.bind(this), 15);
         }
@@ -55,24 +55,38 @@ module feng3d {
             this._scene = value;
         }
 
+        /**
+         * 绘制场景
+         */
         private drawScene() {
 
             this.resize();
             this.renderer.render();
         }
 
+        /**
+         * 重置尺寸
+         */
         private resize() {
 
             if (this.renderWidth != this.canvas.width || this.renderHeight != this.canvas.height) {
 
                 this.renderWidth = this.canvas.width;
                 this.renderHeight = this.canvas.height;
-                this.gl.viewport(0, 0, this.renderWidth, this.renderHeight);
+                this.context3d.viewport(0, 0, this.renderWidth, this.renderHeight);
             }
         }
 
+        /**
+         * 摄像机
+         */
         public get camera(): Camera3D {
             return this._camera;
+        }
+
+        public set camera(value: Camera3D) {
+
+            this._camera = value;
         }
     }
 }
