b522b3bba2666db229b63d29e2835c9586108e87
feng <908087098@qq.com>
2017-04-14 18:20:38 +0800

    制作 变换动画

---
+++ src/animators/transform/TransformAnimator.ts
@@ -0,0 +1,14 @@
+module feng3d
+{
+
+    /**
+     * 变换动作
+     */
+    export class TransformAnimator extends Object3DComponent
+    {
+        /**
+         * 动作名称
+         */
+        public animationName: string;
+    }
+}
