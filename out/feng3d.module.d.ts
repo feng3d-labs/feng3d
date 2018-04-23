24eeb75b7ad600ede68cb4d4297ce348b668a5d8
feng <908087098@qq.com>
2018-04-23 12:35:11 +0800

    编译

--- out/feng3d.module.d.ts
+++ out/feng3d.module.d.ts
@@ -10849,66 +10849,6 @@ declare namespace feng3d {
 }
 declare namespace feng3d {
     /**
-     * FPS模式控制器
-     * @author feng 2016-12-19
-     */
-    class FPSControllerScript extends Behaviour {
-        /**
-         * 加速度
-         */
-        acceleration: number;
-        /**
-         * 按键记录
-         */
-        private keyDownDic;
-        /**
-         * 按键方向字典
-         */
-        private keyDirectionDic;
-        /**
-         * 速度
-         */
-        private velocity;
-        /**
-         * 上次鼠标位置
-         */
-        private preMousePoint;
-        private ischange;
-        private _auto;
-        auto: boolean;
-        init(gameobject: GameObject): void;
-        onMousedown(): void;
-        onMouseup(): void;
-        /**
-         * 销毁
-         */
-        dispose(): void;
-        /**
-         * 手动应用更新到目标3D对象
-         */
-        update(): void;
-        private mousePoint;
-        /**
-         * 处理鼠标移动事件
-         */
-        private onMouseMove(event);
-        /**
-         * 键盘按下事件
-         */
-        private onKeydown(event);
-        /**
-         * 键盘弹起事件
-         */
-        private onKeyup(event);
-        /**
-         * 停止xyz方向运动
-         * @param direction     停止运动的方向
-         */
-        private stopDirectionVelocity(direction);
-    }
-}
-declare namespace feng3d {
-    /**
      * 运行环境枚举
      */
     enum RunEnvironment {
