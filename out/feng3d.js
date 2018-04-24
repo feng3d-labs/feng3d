656bd4116ed3619090179368d488bf8ed1fbca2b
feng <908087098@qq.com>
2018-04-24 17:25:30 +0800

    调整 Engine.camera

--- out/feng3d.js
+++ out/feng3d.js
@@ -13599,11 +13599,38 @@ var feng3d;
             feng3d.debuger && feng3d.assert(canvas instanceof HTMLCanvasElement, "canvas\u53C2\u6570\u5FC5\u987B\u4E3A HTMLCanvasElement \u7C7B\u578B\uFF01");
             this.canvas = canvas;
             this.scene = scene || feng3d.GameObject.create("scene").addComponent(feng3d.Scene3D);
-            this.camera = camera || feng3d.GameObject.create("camera").addComponent(feng3d.Camera);
+            this.camera = camera;
+            this.scene.gameObject.addChild(this.camera.gameObject);
             this.start();
             this.renderContext = new feng3d.RenderContext();
             this.mouse3DManager = new feng3d.Mouse3DManager(canvas);
         }
+        Object.defineProperty(Engine.prototype, "camera", {
+            /**
+             * 摄像机
+             */
+            get: function () {
+                var cameras = this.scene.getComponentsInChildren(feng3d.Camera);
+                if (cameras.length == 0) {
+                    var cam = feng3d.GameObject.create("defaultCamera").addComponent(feng3d.Camera);
+                    this.scene.gameObject.addChild(cam.gameObject);
+                    return cam;
+                }
+                else if (cameras.length == 1) {
+                    return cameras[0];
+                }
+                if (cameras[1].gameObject.name == "defaultCamera") {
+                    return cameras[0];
+                }
+                return cameras[1];
+            },
+            set: function (v) {
+                if (v)
+                    this.scene.gameObject.addChild(v.gameObject);
+            },
+            enumerable: true,
+            configurable: true
+        });
         Object.defineProperty(Engine.prototype, "root", {
             /**
              * 根节点
