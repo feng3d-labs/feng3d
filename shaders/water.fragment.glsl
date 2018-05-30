73660c96ba00da22e084d691210deff3095d85f4
feng <908087098@qq.com>
2018-05-30 10:47:09 +0800

    制作 水 shader

--- shaders/water.fragment.glsl
+++ shaders/water.fragment.glsl
@@ -1,16 +1,19 @@
 uniform sampler2D mirrorSampler;
-uniform float alpha;
-uniform float u_time;
-uniform float size;
-uniform float distortionScale;
 uniform sampler2D normalSampler;
-uniform vec3 sunColor;
-uniform vec3 sunDirection;
-uniform vec3 eye;
-uniform vec3 waterColor;
+
+uniform mat4 u_cameraMatrix;
 
 varying vec4 v_mirrorCoord;
 varying vec4 v_worldPosition;
+
+uniform float u_alpha;
+uniform float u_time;
+uniform float u_size;
+uniform float u_distortionScale;
+uniform vec3 u_sunColor;
+uniform vec3 u_sunDirection;
+uniform vec3 u_waterColor;
+
 
 vec4 getNoise( vec2 uv ) {
 	vec2 uv0 = ( uv / 103.0 ) + vec2(u_time / 17.0, u_time / 29.0);
@@ -25,28 +28,28 @@ vec4 getNoise( vec2 uv ) {
 }
 
 void sunLight( const vec3 surfaceNormal, const vec3 eyeDirection, float shiny, float spec, float diffuse, inout vec3 diffuseColor, inout vec3 specularColor ) {
-	vec3 reflection = normalize( reflect( -sunDirection, surfaceNormal ) );
+	vec3 reflection = normalize( reflect( -u_sunDirection, surfaceNormal ) );
 	float direction = max( 0.0, dot( eyeDirection, reflection ) );
-	specularColor += pow( direction, shiny ) * sunColor * spec;
-	diffuseColor += max( dot( sunDirection, surfaceNormal ), 0.0 ) * sunColor * diffuse;
+	specularColor += pow( direction, shiny ) * u_sunColor * spec;
+	diffuseColor += max( dot( u_sunDirection, surfaceNormal ), 0.0 ) * u_sunColor * diffuse;
 }
 
 void main() {
-	vec4 noise = getNoise( v_worldPosition.xz * size );
+	vec4 noise = getNoise( v_worldPosition.xz * u_size );
 	vec3 surfaceNormal = normalize( noise.xzy * vec3( 1.5, 1.0, 1.5 ) );
 	vec3 diffuseLight = vec3(0.0);
 	vec3 specularLight = vec3(0.0);
-	vec3 worldToEye = eye-v_worldPosition.xyz;
+	vec3 worldToEye = u_cameraMatrix[3].xyz-v_worldPosition.xyz;
 	vec3 eyeDirection = normalize( worldToEye );
 	sunLight( surfaceNormal, eyeDirection, 100.0, 2.0, 0.5, diffuseLight, specularLight );
 	float distance = length(worldToEye);
-	vec2 distortion = surfaceNormal.xz * ( 0.001 + 1.0 / distance ) * distortionScale;
+	vec2 distortion = surfaceNormal.xz * ( 0.001 + 1.0 / distance ) * u_distortionScale;
 	vec3 reflectionSample = vec3( texture2D( mirrorSampler, v_mirrorCoord.xy / v_mirrorCoord.z + distortion ) );
 	float theta = max( dot( eyeDirection, surfaceNormal ), 0.0 );
 	float rf0 = 0.3;
 	float reflectance = rf0 + ( 1.0 - rf0 ) * pow( ( 1.0 - theta ), 5.0 );
-	vec3 scatter = max( 0.0, dot( surfaceNormal, eyeDirection ) ) * waterColor;
-	vec3 albedo = mix( ( sunColor * diffuseLight * 0.3 + scatter ) * getShadowMask(), ( vec3( 0.1 ) + reflectionSample * 0.9 + reflectionSample * specularLight ), reflectance);
+	vec3 scatter = max( 0.0, dot( surfaceNormal, eyeDirection ) ) * u_waterColor;
+	vec3 albedo = mix( ( u_sunColor * diffuseLight * 0.3 + scatter ) * getShadowMask(), ( vec3( 0.1 ) + reflectionSample * 0.9 + reflectionSample * specularLight ), reflectance);
 	vec3 outgoingLight = albedo;
-	gl_FragColor = vec4( outgoingLight, alpha );
+	gl_FragColor = vec4( outgoingLight, u_alpha );
 }
