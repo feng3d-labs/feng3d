605ae07abd8b52a3f47a458a7522196b7c906e1d
feng <908087098@qq.com>
2018-07-27 00:04:18 +0800

    添加 点光源 阴影 shader

--- shaders/modules/lightShading.fragment.glsl
+++ shaders/modules/lightShading.fragment.glsl
@@ -229,6 +229,116 @@ float getShadow( sampler2D shadowMap, int shadowType, vec2 shadowMapSize, float 
     return shadow;
 }
 
+// cubeToUV() maps a 3D direction vector suitable for cube texture mapping to a 2D
+// vector suitable for 2D texture mapping. This code uses the following layout for the
+// 2D texture:
+//
+// xzXZ
+//  y Y
+//
+// Y - Positive y direction
+// y - Negative y direction
+// X - Positive x direction
+// x - Negative x direction
+// Z - Positive z direction
+// z - Negative z direction
+//
+// Source and test bed:
+// https://gist.github.com/tschw/da10c43c467ce8afd0c4
+
+vec2 cubeToUV( vec3 v, float texelSizeY ) {
+
+    // Number of texels to avoid at the edge of each square
+
+    vec3 absV = abs( v );
+
+    // Intersect unit cube
+
+    float scaleToCube = 1.0 / max( absV.x, max( absV.y, absV.z ) );
+    absV *= scaleToCube;
+
+    // Apply scale to avoid seams
+
+    // two texels less per square (one texel will do for NEAREST)
+    v *= scaleToCube * ( 1.0 - 2.0 * texelSizeY );
+
+    // Unwrap
+
+    // space: -1 ... 1 range for each square
+    //
+    // #X##		dim    := ( 4 , 2 )
+    //  # #		center := ( 1 , 1 )
+
+    vec2 planar = v.xy;
+
+    float almostATexel = 1.5 * texelSizeY;
+    float almostOne = 1.0 - almostATexel;
+
+    if ( absV.z >= almostOne ) {
+
+        if ( v.z > 0.0 )
+            planar.x = 4.0 - v.x;
+
+    } else if ( absV.x >= almostOne ) {
+
+        float signX = sign( v.x );
+        planar.x = v.z * signX + 2.0 * signX;
+
+    } else if ( absV.y >= almostOne ) {
+
+        float signY = sign( v.y );
+        planar.x = v.x + 2.0 * signY + 2.0;
+        planar.y = v.z * signY - 2.0;
+
+    }
+
+    // Transform to UV space
+
+    // scale := 0.5 / dim
+    // translate := ( center + 0.5 ) / dim
+    return vec2( 0.125, 0.25 ) * planar + vec2( 0.375, 0.75 );
+
+}
+
+float getPointShadow( sampler2D shadowMap, vec2 shadowMapSize, float shadowBias, float shadowRadius, vec4 shadowCoord, float shadowCameraNear, float shadowCameraFar ) {
+
+    vec2 texelSize = vec2( 1.0 ) / ( shadowMapSize * vec2( 4.0, 2.0 ) );
+
+    // for point lights, the uniform @vShadowCoord is re-purposed to hold
+    // the vector from the light to the world-space position of the fragment.
+    vec3 lightToPosition = shadowCoord.xyz;
+
+    // dp = normalized distance from light to fragment position
+    float dp = ( length( lightToPosition ) - shadowCameraNear ) / ( shadowCameraFar - shadowCameraNear ); // need to clamp?
+    dp += shadowBias;
+
+    // bd3D = base direction 3D
+    vec3 bd3D = normalize( lightToPosition );
+
+    #if defined( SHADOWMAP_TYPE_PCF ) || defined( SHADOWMAP_TYPE_PCF_SOFT )
+
+        vec2 offset = vec2( - 1, 1 ) * shadowRadius * texelSize.y;
+
+        return (
+            texture2DCompare( shadowMap, cubeToUV( bd3D + offset.xyy, texelSize.y ), dp ) +
+            texture2DCompare( shadowMap, cubeToUV( bd3D + offset.yyy, texelSize.y ), dp ) +
+            texture2DCompare( shadowMap, cubeToUV( bd3D + offset.xyx, texelSize.y ), dp ) +
+            texture2DCompare( shadowMap, cubeToUV( bd3D + offset.yyx, texelSize.y ), dp ) +
+            texture2DCompare( shadowMap, cubeToUV( bd3D, texelSize.y ), dp ) +
+            texture2DCompare( shadowMap, cubeToUV( bd3D + offset.xxy, texelSize.y ), dp ) +
+            texture2DCompare( shadowMap, cubeToUV( bd3D + offset.yxy, texelSize.y ), dp ) +
+            texture2DCompare( shadowMap, cubeToUV( bd3D + offset.xxx, texelSize.y ), dp ) +
+            texture2DCompare( shadowMap, cubeToUV( bd3D + offset.yxx, texelSize.y ), dp )
+        ) * ( 1.0 / 9.0 );
+
+    #else // no percentage-closer filtering
+
+        return texture2DCompare( shadowMap, cubeToUV( bd3D, texelSize.y ), dp );
+
+    #endif
+
+}
+
 //渲染点光源
 vec3 lightShading(vec3 normal,vec3 diffuseColor,vec3 specularColor,vec3 ambientColor,float glossiness){
 
@@ -237,29 +347,52 @@ vec3 lightShading(vec3 normal,vec3 diffuseColor,vec3 specularColor,vec3 ambientC
 
     vec3 totalDiffuseLightColor = vec3(0.0,0.0,0.0);
     vec3 totalSpecularLightColor = vec3(0.0,0.0,0.0);
-    #ifdef NUM_POINTLIGHT
-        #if NUM_POINTLIGHT > 0
-            PointLight pointLight;
-            for(int i = 0;i<NUM_POINTLIGHT;i++){
-                pointLight = u_pointLights[i];
-                //
-                vec3 lightOffset = pointLight.position - v_worldPosition;
-                float lightDistance = length(lightOffset);
-                //光照方向
-                vec3 lightDir = normalize(lightOffset);
-                //灯光颜色
-                vec3 lightColor = pointLight.color;
-                //灯光强度
-                float lightIntensity = pointLight.intensity;
-                //光照范围
-                float range = pointLight.range;
-                float attenuation = computeDistanceLightFalloff(lightDistance,range);
-                lightIntensity = lightIntensity * attenuation;
-                //
-                totalDiffuseLightColor = totalDiffuseLightColor +  calculateLightDiffuse(normal,lightDir) * lightColor * lightIntensity;
-                totalSpecularLightColor = totalSpecularLightColor +  calculateLightSpecular(normal,lightDir,viewDir,glossiness) * lightColor * lightIntensity;
-            }
-        #endif
+    #if NUM_POINTLIGHT > 0
+        PointLight pointLight;
+        for(int i = 0;i<NUM_POINTLIGHT;i++){
+            pointLight = u_pointLights[i];
+            //
+            vec3 lightOffset = pointLight.position - v_worldPosition;
+            float lightDistance = length(lightOffset);
+            //光照方向
+            vec3 lightDir = normalize(lightOffset);
+            //灯光颜色
+            vec3 lightColor = pointLight.color;
+            //灯光强度
+            float lightIntensity = pointLight.intensity;
+            //光照范围
+            float range = pointLight.range;
+            float attenuation = computeDistanceLightFalloff(lightDistance,range);
+            lightIntensity = lightIntensity * attenuation;
+            //
+            totalDiffuseLightColor = totalDiffuseLightColor +  calculateLightDiffuse(normal,lightDir) * lightColor * lightIntensity;
+            totalSpecularLightColor = totalSpecularLightColor +  calculateLightSpecular(normal,lightDir,viewDir,glossiness) * lightColor * lightIntensity;
+        }
+    #endif
+
+    #if NUM_POINTLIGHT_CASTSHADOW > 0
+        CastShadowPointLight castShadowPointLight;
+        for(int i = 0;i<NUM_POINTLIGHT_CASTSHADOW;i++){
+            castShadowPointLight = u_castShadowPointLights[i];
+            //
+            vec3 lightOffset = castShadowPointLight.position - v_worldPosition;
+            float lightDistance = length(lightOffset);
+            //光照方向
+            vec3 lightDir = normalize(lightOffset);
+            //灯光颜色
+            vec3 lightColor = castShadowPointLight.color;
+            //灯光强度
+            float lightIntensity = castShadowPointLight.intensity;
+            //光照范围
+            float range = castShadowPointLight.range;
+            float attenuation = computeDistanceLightFalloff(lightDistance,range);
+            lightIntensity = lightIntensity * attenuation;
+            // 计算阴影
+            float shadow = getPointShadow( u_pointShadowMaps[ i ], castShadowPointLight.shadowMapSize, castShadowPointLight.shadowBias, castShadowPointLight.shadowRadius, v_pointShadowCoord[ i ], castShadowPointLight.shadowCameraNear, castShadowPointLight.shadowCameraFar );
+            //
+            totalDiffuseLightColor = totalDiffuseLightColor +  calculateLightDiffuse(normal,lightDir) * lightColor * lightIntensity * shadow;
+            totalSpecularLightColor = totalSpecularLightColor +  calculateLightSpecular(normal,lightDir,viewDir,glossiness) * lightColor * lightIntensity * shadow;
+        }
     #endif
 
     #if NUM_DIRECTIONALLIGHT > 0
