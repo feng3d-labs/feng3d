84eb14f19b2288ea37eff4178024f36d248e568f
feng <908087098@qq.com>
2018-10-01 18:06:04 +0800

    调整 shaders

--- shaders/terrain.vertex.glsl
+++ shaders/terrain.vertex.glsl
@@ -1,45 +1,26 @@
 precision mediump float;  
 
-//坐标属性
-attribute vec3 a_position;
-attribute vec2 a_uv;
-attribute vec3 a_normal;
-
-uniform mat4 u_modelMatrix;
-uniform mat4 u_ITModelMatrix;
-uniform mat4 u_viewProjection;
-
-varying vec2 v_uv;
-varying vec3 v_worldPosition;
-varying vec3 v_normal;
-
-attribute vec3 a_tangent;
-
-varying vec3 v_tangent;
-varying vec3 v_bitangent;
-
+#include<attribute_pars_vert>
+//
+#include<worldposition_pars_vert>
+#include<project_pars_vert>
+//
+#include<uv_pars_vert>
+#include<normalmap_pars_vert>
+//
 #include<lights_pars_vert>
 #include<pointsize_pars_vert>
 
 void main() 
 {
-    vec4 position = vec4(a_position, 1.0);
-
-    vec3 normal = a_normal;
-
-    //获取全局坐标
-    vec4 worldPosition = u_modelMatrix * position;
-    //计算投影坐标
-    gl_Position = u_viewProjection * worldPosition;
-    //输出全局坐标
-    v_worldPosition = worldPosition.xyz;
-    //输出uv
-    v_uv = a_uv;
-
-    //计算法线
-    v_normal = normalize((u_ITModelMatrix * vec4(normal, 0.0)).xyz);
-    v_tangent = normalize((u_modelMatrix * vec4(a_tangent, 0.0)).xyz);
-    v_bitangent = cross(v_normal, v_tangent);
+    // 初始化
+    #include<attribute_vert>
+    // 投影
+    #include<worldposition_vert>
+    #include<project_vert>
+    // 
+    #include<uv_vert>
+    #include<normalmap_vert>
     
     #include<lights_vert>
     #include<pointsize_vert>
