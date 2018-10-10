068feaa00dbf6b4cc7a5dc4db3b0f2ce81375187
feng <908087098@qq.com>
2018-10-10 23:29:42 +0800

    调整 particle_pars_vert.glsl，支持粒子旋转效果

--- shaders/modules/particle_pars_vert.glsl
+++ shaders/modules/particle_pars_vert.glsl
@@ -9,9 +9,36 @@
 
     varying vec4 v_particle_color;
 
+    mat3 makeParticleRotationMatrix(vec3 rotation)
+    {
+        float DEG2RAD = 3.1415926 / 180.0;
+        
+        float rx = rotation.x * DEG2RAD;
+        float ry = rotation.y * DEG2RAD;
+        float rz = rotation.z * DEG2RAD;
+
+        float sx = sin(rx);
+        float cx = cos(rx);
+        float sy = sin(ry);
+        float cy = cos(ry);
+        float sz = sin(rz);
+        float cz = cos(rz);
+
+        mat3 tmp;
+        tmp[ 0 ] = vec3(cy * cz, cy * sz, -sy);
+        tmp[ 1 ] = vec3(sx * sy * cz - cx * sz, sx * sy * sz + cx * cz, sx * cy);
+        tmp[ 2 ] = vec3(cx * sy * cz + sx * sz, cx * sy * sz - sx * cz, cx * cy);
+        return tmp;
+    }
+
     vec4 particleAnimation(vec4 position) 
     {
+        // 计算缩放
         position.xyz = position.xyz * a_particle_scale;
+
+        // 计算旋转
+        mat3 rMat = makeParticleRotationMatrix(a_particle_rotation);
+        position.xyz = rMat * position.xyz;
 
         // 位移
         position = u_particle_billboardMatrix * position;
