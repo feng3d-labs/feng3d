43cb17c48dc9ebeb078f1915f74a8d89f5f14acb
feng <908087098@qq.com>
2018-11-27 11:12:15 +0800

    制作 OVTerrain

--- src/terrain/Terrain.ts
+++ src/terrain/Terrain.ts
@@ -6,6 +6,7 @@ namespace feng3d
     /**
      * The Terrain component renders the terrain.
      */
+    @ov({ component: "OVTerrain" })
     export class Terrain extends Model
     {
         __class__: "feng3d.Terrain" = "feng3d.Terrain";
