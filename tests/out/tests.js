c9c8311de68e902cae6290cc2ca9f4f905f1569c
feng <908087098@qq.com>
2019-01-04 17:10:18 +0800

    新增 MaxHeap.tests

--- tests/out/tests.js
+++ tests/out/tests.js
@@ -603,6 +603,279 @@ QUnit.module("LinkedList", function () {
         assert.ok(ll.checkStructure());
     });
 });
+QUnit.module("MaxHeap", function () {
+    QUnit.test("MaxHeap", function (assert) {
+        var maxHeap = new ds.MaxHeap();
+        assert.deepEqual(maxHeap.peek(), null);
+        assert.deepEqual(maxHeap.isEmpty(), true);
+    });
+    QUnit.test("should add items to the heap and heapify it up", function (assert) {
+        var maxHeap = new ds.MaxHeap();
+        maxHeap.add(5);
+        assert.deepEqual(maxHeap.isEmpty(), false);
+        assert.deepEqual(maxHeap.peek(), 5);
+        assert.deepEqual(maxHeap.toString(), '5');
+        maxHeap.add(3);
+        assert.deepEqual(maxHeap.peek(), 5);
+        assert.deepEqual(maxHeap.toString(), '5,3');
+        maxHeap.add(10);
+        assert.deepEqual(maxHeap.peek(), 10);
+        assert.deepEqual(maxHeap.toString(), '10,3,5');
+        maxHeap.add(1);
+        assert.deepEqual(maxHeap.peek(), 10);
+        assert.deepEqual(maxHeap.toString(), '10,3,5,1');
+        maxHeap.add(1);
+        assert.deepEqual(maxHeap.peek(), 10);
+        assert.deepEqual(maxHeap.toString(), '10,3,5,1,1');
+        assert.deepEqual(maxHeap.poll(), 10);
+        assert.deepEqual(maxHeap.toString(), '5,3,1,1');
+        assert.deepEqual(maxHeap.poll(), 5);
+        assert.deepEqual(maxHeap.toString(), '3,1,1');
+        assert.deepEqual(maxHeap.poll(), 3);
+        assert.deepEqual(maxHeap.toString(), '1,1');
+    });
+    QUnit.test('should poll items from the heap and heapify it down', function (assert) {
+        var maxHeap = new ds.MaxHeap();
+        maxHeap.add(5);
+        maxHeap.add(3);
+        maxHeap.add(10);
+        maxHeap.add(11);
+        maxHeap.add(1);
+        assert.deepEqual(maxHeap.toString(), '11,10,5,3,1');
+        assert.deepEqual(maxHeap.poll(), 11);
+        assert.deepEqual(maxHeap.toString(), '10,3,5,1');
+        assert.deepEqual(maxHeap.poll(), 10);
+        assert.deepEqual(maxHeap.toString(), '5,3,1');
+        assert.deepEqual(maxHeap.poll(), 5);
+        assert.deepEqual(maxHeap.toString(), '3,1');
+        assert.deepEqual(maxHeap.poll(), 3);
+        assert.deepEqual(maxHeap.toString(), '1');
+        assert.deepEqual(maxHeap.poll(), 1);
+        assert.deepEqual(maxHeap.toString(), '');
+        assert.deepEqual(maxHeap.poll(), null);
+        assert.deepEqual(maxHeap.toString(), '');
+    });
+    QUnit.test('should heapify down through the right branch as well', function (assert) {
+        var maxHeap = new ds.MaxHeap();
+        maxHeap.add(3);
+        maxHeap.add(12);
+        maxHeap.add(10);
+        assert.deepEqual(maxHeap.toString(), '12,3,10');
+        maxHeap.add(11);
+        assert.deepEqual(maxHeap.toString(), '12,11,10,3');
+        assert.deepEqual(maxHeap.poll(), 12);
+        assert.deepEqual(maxHeap.toString(), '11,3,10');
+    });
+    QUnit.test('should be possible to find item indices in heap', function (assert) {
+        var maxHeap = new ds.MaxHeap();
+        maxHeap.add(3);
+        maxHeap.add(12);
+        maxHeap.add(10);
+        maxHeap.add(11);
+        maxHeap.add(11);
+        assert.deepEqual(maxHeap.toString(), '12,11,10,3,11');
+        assert.deepEqual(maxHeap.find(5), []);
+        assert.deepEqual(maxHeap.find(12), [0]);
+        assert.deepEqual(maxHeap.find(11), [1, 4]);
+    });
+    QUnit.test('should be possible to remove items from heap with heapify down', function (assert) {
+        var maxHeap = new ds.MaxHeap();
+        maxHeap.add(3);
+        maxHeap.add(12);
+        maxHeap.add(10);
+        maxHeap.add(11);
+        maxHeap.add(11);
+        assert.deepEqual(maxHeap.toString(), '12,11,10,3,11');
+        assert.deepEqual(maxHeap.remove(12).toString(), '11,11,10,3');
+        assert.deepEqual(maxHeap.remove(12).peek(), 11);
+        assert.deepEqual(maxHeap.remove(11).toString(), '10,3');
+        assert.deepEqual(maxHeap.remove(10).peek(), 3);
+    });
+    QUnit.test('should be possible to remove items from heap with heapify up', function (assert) {
+        var maxHeap = new ds.MaxHeap();
+        maxHeap.add(3);
+        maxHeap.add(10);
+        maxHeap.add(5);
+        maxHeap.add(6);
+        maxHeap.add(7);
+        maxHeap.add(4);
+        maxHeap.add(6);
+        maxHeap.add(8);
+        maxHeap.add(2);
+        maxHeap.add(1);
+        assert.deepEqual(maxHeap.toString(), '10,8,6,7,6,4,5,3,2,1');
+        assert.deepEqual(maxHeap.remove(4).toString(), '10,8,6,7,6,1,5,3,2');
+        assert.deepEqual(maxHeap.remove(3).toString(), '10,8,6,7,6,1,5,2');
+        assert.deepEqual(maxHeap.remove(5).toString(), '10,8,6,7,6,1,2');
+        assert.deepEqual(maxHeap.remove(10).toString(), '8,7,6,2,6,1');
+        assert.deepEqual(maxHeap.remove(6).toString(), '8,7,1,2');
+        assert.deepEqual(maxHeap.remove(2).toString(), '8,7,1');
+        assert.deepEqual(maxHeap.remove(1).toString(), '8,7');
+        assert.deepEqual(maxHeap.remove(7).toString(), '8');
+        assert.deepEqual(maxHeap.remove(8).toString(), '');
+    });
+    QUnit.test('should be possible to remove items from heap with custom finding comparator', function (assert) {
+        var maxHeap = new ds.MaxHeap();
+        maxHeap.add('a');
+        maxHeap.add('bb');
+        maxHeap.add('ccc');
+        maxHeap.add('dddd');
+        assert.deepEqual(maxHeap.toString(), 'dddd,ccc,bb,a');
+        var comparator = new ds.Comparator(function (a, b) {
+            if (a.length === b.length) {
+                return 0;
+            }
+            return a.length < b.length ? -1 : 1;
+        });
+        maxHeap.remove('hey', comparator);
+        assert.deepEqual(maxHeap.toString(), 'dddd,a,bb');
+    });
+});
+QUnit.module('MinHeap', function (assert) {
+    QUnit.test('should create an empty min heap', function (assert) {
+        var minHeap = new ds.MinHeap();
+        assert.deepEqual(minHeap.peek(), null);
+        assert.deepEqual(minHeap.isEmpty(), true);
+    });
+    QUnit.test('should add items to the heap and heapify it up', function (assert) {
+        var minHeap = new ds.MinHeap();
+        minHeap.add(5);
+        assert.deepEqual(minHeap.isEmpty(), false);
+        assert.deepEqual(minHeap.peek(), 5);
+        assert.deepEqual(minHeap.toString(), '5');
+        minHeap.add(3);
+        assert.deepEqual(minHeap.peek(), 3);
+        assert.deepEqual(minHeap.toString(), '3,5');
+        minHeap.add(10);
+        assert.deepEqual(minHeap.peek(), 3);
+        assert.deepEqual(minHeap.toString(), '3,5,10');
+        minHeap.add(1);
+        assert.deepEqual(minHeap.peek(), 1);
+        assert.deepEqual(minHeap.toString(), '1,3,10,5');
+        minHeap.add(1);
+        assert.deepEqual(minHeap.peek(), 1);
+        assert.deepEqual(minHeap.toString(), '1,1,10,5,3');
+        assert.deepEqual(minHeap.poll(), 1);
+        assert.deepEqual(minHeap.toString(), '1,3,10,5');
+        assert.deepEqual(minHeap.poll(), 1);
+        assert.deepEqual(minHeap.toString(), '3,5,10');
+        assert.deepEqual(minHeap.poll(), 3);
+        assert.deepEqual(minHeap.toString(), '5,10');
+    });
+    QUnit.test('should poll items from the heap and heapify it down', function (assert) {
+        var minHeap = new ds.MinHeap();
+        minHeap.add(5);
+        minHeap.add(3);
+        minHeap.add(10);
+        minHeap.add(11);
+        minHeap.add(1);
+        assert.deepEqual(minHeap.toString(), '1,3,10,11,5');
+        assert.deepEqual(minHeap.poll(), 1);
+        assert.deepEqual(minHeap.toString(), '3,5,10,11');
+        assert.deepEqual(minHeap.poll(), 3);
+        assert.deepEqual(minHeap.toString(), '5,11,10');
+        assert.deepEqual(minHeap.poll(), 5);
+        assert.deepEqual(minHeap.toString(), '10,11');
+        assert.deepEqual(minHeap.poll(), 10);
+        assert.deepEqual(minHeap.toString(), '11');
+        assert.deepEqual(minHeap.poll(), 11);
+        assert.deepEqual(minHeap.toString(), '');
+        assert.deepEqual(minHeap.poll(), null);
+        assert.deepEqual(minHeap.toString(), '');
+    });
+    QUnit.test('should heapify down through the right branch as well', function (assert) {
+        var minHeap = new ds.MinHeap();
+        minHeap.add(3);
+        minHeap.add(12);
+        minHeap.add(10);
+        assert.deepEqual(minHeap.toString(), '3,12,10');
+        minHeap.add(11);
+        assert.deepEqual(minHeap.toString(), '3,11,10,12');
+        assert.deepEqual(minHeap.poll(), 3);
+        assert.deepEqual(minHeap.toString(), '10,11,12');
+    });
+    QUnit.test('should be possible to find item indices in heap', function (assert) {
+        var minHeap = new ds.MinHeap();
+        minHeap.add(3);
+        minHeap.add(12);
+        minHeap.add(10);
+        minHeap.add(11);
+        minHeap.add(11);
+        assert.deepEqual(minHeap.toString(), '3,11,10,12,11');
+        assert.deepEqual(minHeap.find(5), []);
+        assert.deepEqual(minHeap.find(3), [0]);
+        assert.deepEqual(minHeap.find(11), [1, 4]);
+    });
+    QUnit.test('should be possible to remove items from heap with heapify down', function (assert) {
+        var minHeap = new ds.MinHeap();
+        minHeap.add(3);
+        minHeap.add(12);
+        minHeap.add(10);
+        minHeap.add(11);
+        minHeap.add(11);
+        assert.deepEqual(minHeap.toString(), '3,11,10,12,11');
+        assert.deepEqual(minHeap.remove(3).toString(), '10,11,11,12');
+        assert.deepEqual(minHeap.remove(3).peek(), 10);
+        assert.deepEqual(minHeap.remove(11).toString(), '10,12');
+        assert.deepEqual(minHeap.remove(3).peek(), 10);
+    });
+    QUnit.test('should be possible to remove items from heap with heapify up', function (assert) {
+        var minHeap = new ds.MinHeap();
+        minHeap.add(3);
+        minHeap.add(10);
+        minHeap.add(5);
+        minHeap.add(6);
+        minHeap.add(7);
+        minHeap.add(4);
+        minHeap.add(6);
+        minHeap.add(8);
+        minHeap.add(2);
+        minHeap.add(1);
+        assert.deepEqual(minHeap.toString(), '1,2,4,6,3,5,6,10,8,7');
+        assert.deepEqual(minHeap.remove(8).toString(), '1,2,4,6,3,5,6,10,7');
+        assert.deepEqual(minHeap.remove(7).toString(), '1,2,4,6,3,5,6,10');
+        assert.deepEqual(minHeap.remove(1).toString(), '2,3,4,6,10,5,6');
+        assert.deepEqual(minHeap.remove(2).toString(), '3,6,4,6,10,5');
+        assert.deepEqual(minHeap.remove(6).toString(), '3,5,4,10');
+        assert.deepEqual(minHeap.remove(10).toString(), '3,5,4');
+        assert.deepEqual(minHeap.remove(5).toString(), '3,4');
+        assert.deepEqual(minHeap.remove(3).toString(), '4');
+        assert.deepEqual(minHeap.remove(4).toString(), '');
+    });
+    QUnit.test('should be possible to remove items from heap with custom finding comparator', function (assert) {
+        var minHeap = new ds.MinHeap();
+        minHeap.add('dddd');
+        minHeap.add('ccc');
+        minHeap.add('bb');
+        minHeap.add('a');
+        assert.deepEqual(minHeap.toString(), 'a,bb,ccc,dddd');
+        var comparator = new ds.Comparator(function (a, b) {
+            if (a.length === b.length) {
+                return 0;
+            }
+            return a.length < b.length ? -1 : 1;
+        });
+        minHeap.remove('hey', comparator);
+        assert.deepEqual(minHeap.toString(), 'a,bb,dddd');
+    });
+    QUnit.test('should remove values from heap and correctly re-order the tree', function (assert) {
+        var minHeap = new ds.MinHeap();
+        minHeap.add(1);
+        minHeap.add(2);
+        minHeap.add(3);
+        minHeap.add(4);
+        minHeap.add(5);
+        minHeap.add(6);
+        minHeap.add(7);
+        minHeap.add(8);
+        minHeap.add(9);
+        assert.deepEqual(minHeap.toString(), '1,2,3,4,5,6,7,8,9');
+        minHeap.remove(2);
+        assert.deepEqual(minHeap.toString(), '1,4,3,8,5,6,7,9');
+        minHeap.remove(4);
+        assert.deepEqual(minHeap.toString(), '1,5,3,8,9,6,7');
+    });
+});
 QUnit.module("PriorityQueue", function () {
     QUnit.test("push", function (assert) {
         var arr = ds.utils.createArray(10, function () { return Math.random(); });
