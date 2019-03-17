8d5816d8f04c6255e47c69aacabaf58444d0c8ef
feng <908087098@qq.com>
2019-03-17 23:35:42 +0800

    还原 Task.ts 上次版本

--- src/assets/fs/ReadFS.ts
+++ src/assets/fs/ReadFS.ts
@@ -56,11 +56,6 @@ namespace feng3d
          */
         readStrings(paths: string[], callback: (strs: (string | Error)[]) => void)
         {
-            // var func = <TaskFunction<{ path: string, result: string | Error }>><any>callback;
-            // func.data
-
-            // var taskNodes: TaskNode<{ path: string}>[] = paths.map(v => { return { data: { path: v }, func: callback }; });
-
             Task.createTasks(paths, (path, callback) =>
             {
                 this.readString(path, (err, str) =>
