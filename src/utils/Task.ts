3e87219b28abcd7960602275f7c635c91eee66c0
feng <9080870982qq.com>
2019-03-21 12:38:11 +0800

    还原 错误提交

--- src/utils/Task.ts
+++ src/utils/Task.ts
@@ -6,266 +6,96 @@ namespace feng3d
     export var task: Task;
 
     /**
-     * 任务状态
-     */
-    export enum TaskStatus
-    {
-        /**
-         * 初始状态，未开始
-         */
-        None,
-        /**
-         * 等待状态，等待到依赖任务执行完成
-         */
-        Waiting,
-        /**
-         * 执行状态，进行中
-         */
-        Doing,
-        /**
-         * 完成状态，已完成
-         */
-        Done,
-    }
-
-    /**
      * 任务函数
      */
-    export interface TaskFunction
+    interface TaskFunction
     {
         /**
-         * 默认初始状态，未开始，状态不可逆
+         * 函数自身名称
          */
-        status?: TaskStatus;
-
+        readonly name?: string;
         /**
-         * 任务自身内容回调带回结果
+         * 函数自身
          */
-        result?: any;
-
-        /**
-         * 前置任务列表
-         */
-        preTasks?: TaskFunction[]
-
-        /**
-         * 任务函数自身
-         */
-        (done: (result?: any) => void): void;
-    }
-
-    export interface TF
-    {
         (callback: () => void): void;
     }
 
     /**
-     * 串联
-     * @param fns 
-     */
-    export function parallel(fns: TF[]): TF
-    {
-        var result = (callback) =>
-        {
-            var index = 0;
-
-            var next = (callback) =>
-            {
-                if (index >= fns.length)
-                {
-                    callback && callback();
-                    return;
-                }
-                var fn = fns[index];
-                index++;
-                fn(() =>
-                {
-                    next(callback);
-                });
-            }
-            next(callback);
-        };
-        return result;
-    }
-
-    /**
-     * 串联
-     * @param fns 
-     */
-    export function series(fns: TF[]): TF
-    {
-        var result = (callback) =>
-        {
-            var index = 0;
-            var next = (callback) =>
-            {
-                if (index >= fns.length)
-                {
-                    callback && callback();
-                    return;
-                }
-                var fn = fns[index];
-                index++;
-                var callNum = 0;
-                fn(() =>
-                {
-                    callNum++;
-                    if (callNum == 1) next(callback);
-                    else { console.warn(`${fn.name ? "函数" + fn.name : "匿名函数"} 多次调用回调函数，当前次数 ${callNum}`); }
-                });
-            }
-            next(callback);
-        };
-        return result;
-    }
-
-    /**
-     * 执行任务
+     * 任务
      * 
-     * @param done 完成回调
+     * 处理 异步任务(函数)串联并联执行功能
      */
-    function doTask(task: TaskFunction, done?: () => void)
+    class Task
     {
-        if (task.status == TaskStatus.Done) { done && done(); return; }
-
-        // 回调添加到完成事件中
-        if (done) event.once(task, "done", done);
-
-        // 任务正在执行 直接返回
-        if (task.status == TaskStatus.Doing) return;
-
-        if (task.status == TaskStatus.Waiting)
-        {
-            //判断是否出现等待死锁；例如A前置任务为B，B前置任务为A
-            var waitings = task.preTasks.filter(v => v.status == TaskStatus.Waiting);
-            var index = 0;
-            while (index < waitings.length)
-            {
-                var item = waitings[index];
-                if (item == task)
-                {
-                    console.log(waitings.slice(0, index + 1));
-                    feng3d.error(`出现循环引用任务`)
-                    return;
-                }
-                if (item.preTasks)
-                    waitings = waitings.concat(item.preTasks.filter(v => v.status == TaskStatus.Waiting));
-                index++;
-            }
-            return;
-        }
-
-        // 执行前置任务函数
-        var doPreTasks = (callback: () => void) =>
-        {
-            if (!task.preTasks)
-            {
-                callback();
-                return;
-            }
-            var waitNum = task.preTasks.length;
-            if (waitNum == 0) callback();
-            task.preTasks.forEach(v => doTask(v, () =>
-            {
-                waitNum--;
-                if (waitNum == 0) callback();
-            }));
-        };
-
-        //
-        task.status = TaskStatus.Waiting;
-
-        // 执行前置任务
-        doPreTasks(() =>
-        {
-            task.status = TaskStatus.Doing;
-
-            // 执行任务自身
-            task((result) =>
-            {
-                task.status = TaskStatus.Done;
-                task.result = result;
-                event.dispatch(task, "done");
-            });
-        });
-    }
-
-    /**
-     * 任务，用于处理多件可能有依赖或者嵌套的事情
-     */
-    export class Task
-    {
-        private registerTasks: { [name: string]: { dependencies: string[], fn: TaskFunction } } = {};
-        private tasks: { [name: string]: TaskFunction } = {};
-
         /**
-         * 映射任务名称以及依赖列表
+         * 并联多个异步函数为一个函数
          * 
-         * @param taskName 任务名称
-         * @param dependencies 依赖任务名称列表
-         * @param fn 任务函数
-         */
-        task(taskName: string, dependencies: string[], fn: TaskFunction)
-        {
-            this.registerTasks[taskName] = { dependencies: dependencies, fn: fn };
-        }
-
-        getTask(taskName: string)
-        {
-            if (!this.tasks[taskName]) 
-            {
-                var item = this.registerTasks[taskName];
-                // 获取依赖函数
-                var dependenciesFns = item.dependencies.map(v => this.getTask(v));
-                // 
-                this.tasks[taskName] = this.series([this.parallel(dependenciesFns), item.fn]);
-            }
-
-            return this.tasks[taskName];
-        }
-
-        /**
-         * 把一组无序任务函数并联成一个任务函数，任务之间无依赖关系
+         * 这些异步函数同时执行
          * 
-         * @param fns 任务函数列表
-         * @returns 组合后的任务函数
+         * @param fns 一组异步函数
          */
         parallel(fns: TaskFunction[]): TaskFunction
         {
-            // 构建一组任务
-            var task: TaskFunction = ((done: () => void) => { done(); });
-            task.preTasks = fns;
-            // 完成时提取结果
-            var result = (done: (result?: any) => void) =>
+            var result: TaskFunction = (callback) =>
             {
-                doTask(task, () =>
+                if (fns.length == 0) { callback(); return; }
+                var index = 0;
+                fns.forEach(fn =>
                 {
-                    done(task.result);
+                    var callbackNum = 0;
+                    fn(() =>
+                    {
+                        callbackNum++;
+                        if (callbackNum == 1)
+                        {
+                            index++;
+                            if (index == fns.length)
+                            {
+                                callback();
+                            }
+                        }
+                        else { console.warn(`${fn.name ? "函数" + fn.name : "匿名函数"} 多次调用回调函数，当前次数 ${callbackNum}`); }
+                    });
                 });
             };
             return result;
         }
 
         /**
-         * 把一组有序任务函数串联成一个任务函数，后面函数只在前面函数执行完成后调用
+         * 串联多个异步函数为一个函数
          * 
-         * @param fns 任务函数列表
-         * @param 组合后的任务函数
+         * 这些异步函数按顺序依次执行，等待前一个异步函数执行完调用回调后才执行下一个异步函数。
+         * 
+         * @param fns 一组异步函数
          */
         series(fns: TaskFunction[]): TaskFunction
         {
-            // 构建一组任务
-            var task: TaskFunction = ((done: () => void) => { done(); });
-            task.preTasks = fns;
-            // 串联任务
-            fns.forEach((v, i, arr) => { if (i > 0) arr[i].preTasks = [arr[i - 1]]; });
-            // 完成时提取结果
-            var result = (done: (result?: any) => void) =>
+            var result: TaskFunction = (callback) =>
             {
-                doTask(task, () =>
+                if (fns.length == 0) { callback(); return; }
+                var index = 0;
+                var next = () =>
                 {
-                    done(task.result);
-                });
+                    var fn = fns[index];
+                    var callbackNum = 0;
+                    fn(() =>
+                    {
+                        callbackNum++;
+                        if (callbackNum == 1)
+                        {
+                            index++;
+                            if (index < fns.length)
+                            {
+                                next();
+                            } else
+                            {
+                                callback && callback();
+                            }
+                        }
+                        else { console.warn(`${fn.name ? "函数" + fn.name : "匿名函数"} 多次调用回调函数，当前次数 ${callbackNum}`); }
+                    });
+                }
+                next();
             };
             return result;
         }
@@ -277,24 +107,23 @@ namespace feng3d
          * @param fn 单一任务函数
          * @param done 完成回调
          */
-        parallelResults<P, R>(ps: P[], fn: (p: P, callback: (r: R) => void) => void): (done: (rs: R[]) => void) => void
+        parallelResults<P, R>(ps: P[], fn: (p: P, callback: (r: R) => void) => void, done: (rs: R[]) => void)
         {
-            // 构建一组任务
-            var task: TaskFunction = ((done: () => void) => { done(); });
-            task.preTasks = ps.map(p =>
+            var map = new Map();
+            // 包装函数
+            var fns = ps.map(p => (callback: () => void) =>
             {
-                return (callback) => { fn(p, callback); };
-            });
-            // 完成时提取结果
-            var result = (done: (rs: R[]) => void) =>
-            {
-                doTask(task, () =>
+                fn(p, r =>
                 {
-                    var results = task.preTasks.map(v => v.result);
-                    done(results);
+                    map.set(p, r);
+                    callback();
                 });
-            };
-            return result;
+            });
+            this.parallel(fns)(() =>
+            {
+                var results = ps.map(p => map.get(p));
+                done(results);
+            });
         }
 
         /**
@@ -304,166 +133,23 @@ namespace feng3d
          * @param fn 单一任务函数
          * @param done 完成回调
          */
-        seriesResults<P, R>(ps: P[], fn: (p: P, callback: (r: R) => void) => void): (done: (rs: R[]) => void) => void
+        seriesResults<P, R>(ps: P[], fn: (p: P, callback: (r: R) => void) => void, done: (rs: R[]) => void)
         {
-            // 构建一组任务
-            var task: TaskFunction = ((done: () => void) => { done(); });
-            task.preTasks = ps.map(p =>
+            var map = new Map();
+            // 包装函数
+            var fns = ps.map(p => (callback: () => void) =>
             {
-                return (callback) => { fn(p, callback); };
-            });
-            // 串联任务
-            task.preTasks.forEach((v, i, arr) => { if (i > 0) arr[i].preTasks = [arr[i - 1]]; });
-            // 完成时提取结果
-            var result = (done: (rs: R[]) => void) =>
-            {
-                doTask(task, () =>
+                fn(p, r =>
                 {
-                    var results = task.preTasks.map(v => v.result);
-                    done(results);
+                    map.set(p, r);
+                    callback();
                 });
-            };
-            return result;
-        }
-
-        testParallelResults()
-        {
-            this.parallelResults([1, 2, 3, 4, 5], (p, callback: (r: number) => void) =>
-            {
-                callback(p);
-            })((rs) =>
-            {
-                console.log(rs);
             });
-        }
-
-        testSeriesResults()
-        {
-
-            var starttime = Date.now();
-            this.seriesResults(["tsconfig.json", "index.html",
-                "app.js"], (p, callback) =>
-                {
-                    fs.readString(p, (err, str) =>
-                    {
-                        callback(str);
-                    });
-                })((results: string[]) =>
-                {
-                    console.log(`消耗时间 ${Date.now() - starttime}`);
-                    console.log("succeed");
-                    console.log(`结果:`);
-                    console.log(results);
-                });
-        }
-
-        testParallel()
-        {
-            console.time(`task`);
-            // console.timeStamp(`task`);
-            var funcs: TaskFunction[] = [1, 2, 3, 4].map(v => (callback) =>
+            this.series(fns)(() =>
             {
-                var sleep = 1000 * Math.random();
-                sleep = ~~sleep;
-                console.time(`${sleep}`);
-                setTimeout(() =>
-                {
-                    console.log(v);
-                    console.timeEnd(`${sleep}`);
-                    callback();
-                }, sleep);
+                var results = ps.map(p => map.get(p));
+                done(results);
             });
-            this.parallel(funcs)(() =>
-            {
-                console.log(`done`);
-                console.timeEnd(`task`);
-                // console.timeStamp(`task`);
-            });
-        }
-
-        testSeries()
-        {
-            console.time(`task`);
-            // console.timeStamp(`task`);
-            var funcs: TaskFunction[] = [1, 2, 3, 4].map(v => (callback) =>
-            {
-                var sleep = 1000 * Math.random();
-                sleep = ~~sleep;
-                console.time(`${sleep}`);
-                setTimeout(() =>
-                {
-                    console.log(v);
-                    console.timeEnd(`${sleep}`);
-                    callback();
-                    callback();
-                }, sleep);
-            });
-            var fn = this.series(funcs);
-            fn(() =>
-            {
-                console.log(`done`);
-                console.timeEnd(`task`);
-                // console.timeStamp(`task`);
-            });
-            fn(() =>
-            {
-                console.log(`done`);
-                console.timeEnd(`task`);
-                // console.timeStamp(`task`);
-            });
-        }
-
-        testSeries1()
-        {
-            console.time(`task`);
-            // console.timeStamp(`task`);
-            var funcs: TaskFunction[] = [1, 2, 3, 4].map(v => (callback) =>
-            {
-                var sleep = 1000 * Math.random();
-                sleep = ~~sleep;
-                console.time(`${sleep}`);
-                setTimeout(() =>
-                {
-                    console.log(v);
-                    console.timeEnd(`${sleep}`);
-                    callback();
-                    callback();
-                }, sleep);
-            });
-            var fn = series(funcs);
-            fn(() =>
-            {
-                console.log(`done`);
-                console.timeEnd(`task`);
-                // console.timeStamp(`task`);
-            });
-            // fn(() =>
-            // {
-            //     console.log(`done`);
-            //     console.timeEnd(`task`);
-            //     // console.timeStamp(`task`);
-            // });
-
-        }
-
-        testTask()
-        {
-
-            var fn = function ()
-            {
-                console.log("fn");
-            };
-
-            this.task("copy-html", [], fn);
-
-            this.getTask("copy-html")(() => { });
-            this.getTask("copy-html")(() => { });
-
-            var fn1 = this.getTask("copy-html");
-            var fn2 = this.getTask("copy-html");
-
-            console.log(fn == fn1);
-            console.log(fn2 == fn1);
         }
     }
 
