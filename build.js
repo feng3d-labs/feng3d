22a3f7fc662ecb9c083db41fe6c8f6cfa5dffb60
feng <908087098@qq.com>
2019-04-18 20:33:25 +0800

    还原 event

--- build.js
+++ build.js
@@ -4,19 +4,15 @@ var fs = require("fs");
 var path = require("path");
 
 
-var packagesPath = path.join(__dirname, "packages");
-var packages = fs.readdirSync(packagesPath);
-var packagesProject = packages.map(v => { return { path: path.join(packagesPath, v), moduleName: `@feng3d/${v}`, globalModule: "feng3d" } });
-
 var toolsPath = path.join(__dirname, "tools");
 var tools = fs.readdirSync(toolsPath);
 var toolsProject = tools.map(v => { return { path: path.join(toolsPath, v) } });
 
-var bundlesPath = path.join(__dirname, "bundles");
-var bundles = fs.readdirSync(bundlesPath);
-var bundlesProject = bundles.map(v => { return { path: path.join(bundlesPath, v), moduleName: `feng3d`, globalModule: "feng3d" } });
-
-var projectPaths = packagesProject.concat(toolsProject).concat(bundlesProject);
+var projectPaths = toolsProject.concat({
+    path: __dirname,
+    moduleName: "feng3d",
+    globalModule: "feng3d",
+});
 
 /**
  * Watch for changes in TypeScript
