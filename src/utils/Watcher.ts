38afe9471cf6d9d2b1caa0ee5cf369341734a351
feng <908087098@qq.com>
2019-05-14 14:42:30 +0800

    调整 Watcher

--- src/utils/Watcher.ts
+++ src/utils/Watcher.ts
@@ -5,64 +5,10 @@ namespace feng3d
      * 观察装饰器，观察被装饰属性的变化
      * 
      * @param onChange 属性变化回调  例如参数为“onChange”时，回调将会调用this.onChange(property, oldValue, newValue)
-     * @see https://gitee.com/feng3d/feng3d/issues/IGIK0
      * 
      * 使用@watch后会自动生成一个带"_"的属性，例如 属性"a"会生成"_a" 
      * 
      * 通过使用 eval 函数 生成出 与自己手动写的set get 一样的函数，性能已经接近 手动写的get set函数。
-     * 
-     * 性能：
-     * chrome：
-     * 测试 get ：
-Test.ts:100 watch与getset最大耗时比 1.2222222222222223
-Test.ts:101 watch与getset最小耗时比 0.7674418604651163
-Test.ts:102 watch与getset平均耗时比 0.9558823529411765
-Test.ts:103 watch平均耗时比 13
-Test.ts:104 getset平均耗时比 13.6
-Test.ts:98 测试 set ：
-Test.ts:100 watch与getset最大耗时比 4.5
-Test.ts:101 watch与getset最小耗时比 2.409090909090909
-Test.ts:102 watch与getset平均耗时比 3.037037037037037
-Test.ts:103 watch平均耗时比 57.4
-Test.ts:104 getset平均耗时比 18.9
-
-     * 
-     * nodejs:
-     * 测试 get ：
-watch与getset最大耗时比 1.3333333333333333
-watch与getset最小耗时比 0.55
-watch与getset平均耗时比 1.0075757575757576
-watch平均耗时比 13.3
-getset平均耗时比 13.2
-测试 set ：
-watch与getset最大耗时比 4.9
-watch与getset最小耗时比 3
-watch与getset平均耗时比 4.143497757847534
-watch平均耗时比 92.4
-getset平均耗时比 22.3
-     * 
-     * 
-     * firefox:
-     * 测试 get ：  Test.js:122:5
-watch与getset最大耗时比 4.142857142857143  Test.js:124:5
-watch与getset最小耗时比 0.4090909090909091  Test.js:125:5
-watch与getset平均耗时比 1.0725806451612903  Test.js:126:5
-watch平均耗时比 13.3  Test.js:127:5
-getset平均耗时比 12.4  Test.js:128:5
-测试 set ：  Test.js:122:5
-watch与getset最大耗时比 1.5333333333333334  Test.js:124:5
-watch与getset最小耗时比 0.6842105263157895  Test.js:125:5
-watch与getset平均耗时比 0.9595375722543352  Test.js:126:5
-watch平均耗时比 16.6  Test.js:127:5
-getset平均耗时比 17.3
-     * 
-     * 结果分析：
-     * chrome、nodejs、firefox运行结果出现差异,firefox运行结果最完美
-     * 
-     * 使用watch后的get测试的消耗与手动写get消耗一致
-     * chrome与nodejs上set消耗是手动写set的消耗(3-4)倍
-     * 
-     * 注：不适用eval的情况下，chrome表现最好的，与此次测试结果差不多；在nodejs与firfox上将会出现比使用eval情况下消耗的（40-400）倍，其中详细原因不明，求高人解释！
      * 
      */
     export function watch(onChange: string)
@@ -70,26 +16,14 @@ getset平均耗时比 17.3
         return (target: any, property: string) =>
         {
             var key = "_" + property;
-            var get;
-            // get = function () { return this[key]; };
-            eval(`get = function (){return this.${key}}`);
-
-            var set;
-            // set = function (value)
-            // {
-            //     if (this[key] === value)
-            //         return;
-            //     var oldValue = this[key];
-            //     this[key] = value;
-            //     this[onChange](propertyKey, oldValue, this[key]);
-            // };
-            eval(`set = function (value){
+            var get = eval(`(function (){return this.${key}})`);
+            var set = eval(`(function (value){
                 if (this.${key} == value)
                     return;
                 var oldValue = this.${key};
                 this.${key} = value;
                 this.${onChange}("${property}", oldValue, value);
-            }`);
+            })`);
 
             Object.defineProperty(target, property, {
                 get: get,
@@ -110,9 +44,9 @@ getset平均耗时比 17.3
          * 注意：使用watch后获取该属性值的性能将会是原来的1/60，避免在运算密集处使用该函数。
          * 
          * @param host 被监听对象
-         * @param property 
-         * @param handler 
-         * @param thisObject 
+         * @param property 被监听属性
+         * @param handler 变化回调
+         * @param thisObject 变化回调函数 this值
          */
         watch<T, K extends (keyof T & string), V extends T[K]>(host: T, property: K, handler: (host: T, property: string, oldvalue: V) => void, thisObject?: any)
         {
@@ -177,14 +111,21 @@ getset平均耗时比 17.3
                 propertywatchs.handlers.push({ handler: handler, thisObject: thisObject });
         }
 
-        unwatch<T, K extends keyof T, V extends T[K]>(host: T, property: K, handler?: (host: T, property: string, oldvalue: V) => void, thisObject?: any)
+        /**
+         * 取消监听对象属性的变化
+         * 
+         * @param host 被监听对象
+         * @param property 被监听属性
+         * @param handler 变化回调
+         * @param thisObject 变化回调函数 this值
+         */
+        unwatch<T, K extends (keyof T & string), V extends T[K]>(host: T, property: K, handler?: (host: T, property: string, oldvalue: V) => void, thisObject?: any)
         {
             var watchs: Watchs = host[__watchs__];
             if (!watchs) return;
-            var property1 = <string>property;
-            if (watchs[property1])
+            if (watchs[property])
             {
-                var handlers = watchs[property1].handlers;
+                var handlers = watchs[property].handlers;
                 if (handler === undefined)
                     handlers.length = 0;
                 for (var i = handlers.length - 1; i >= 0; i--)
@@ -194,12 +135,12 @@ getset平均耗时比 17.3
                 }
                 if (handlers.length == 0)
                 {
-                    var value = host[property1];
-                    delete host[property1];
-                    if (watchs[property1].oldPropertyDescriptor)
-                        Object.defineProperty(host, property1, watchs[property1].oldPropertyDescriptor);
-                    host[property1] = value;
-                    delete watchs[property1];
+                    var value = host[property];
+                    delete host[property];
+                    if (watchs[property].oldPropertyDescriptor)
+                        Object.defineProperty(host, property, watchs[property].oldPropertyDescriptor);
+                    host[property] = value;
+                    delete watchs[property];
                 }
                 if (Object.keys(watchs).length == 0)
                 {
@@ -208,6 +149,14 @@ getset平均耗时比 17.3
             }
         }
 
+        /**
+         * 监听对象属性链值变化
+         * 
+         * @param host 被监听对象
+         * @param property 被监听属性
+         * @param handler 变化回调
+         * @param thisObject 变化回调函数 this值
+         */
         watchchain(host: any, property: string, handler?: (host: any, property: string, oldvalue: any) => void, thisObject?: any)
         {
             var notIndex = property.indexOf(".");
