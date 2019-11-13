f1bb4a1eb677299812ad2ddbedcb54eab71d4434
feng <908087098@qq.com>
2019-11-13 11:45:39 +0800

    调整 ParticleEmissionModule 接口

--- src/animators/particle/modules/ParticleEmissionModule.ts
+++ src/animators/particle/modules/ParticleEmissionModule.ts
@@ -1,7 +1,7 @@
 namespace feng3d
 {
     /**
-     * 粒子发射器
+     * 粒子系统发射模块。
      */
     export class ParticleEmissionModule extends ParticleModule
     {
@@ -13,12 +13,50 @@ namespace feng3d
         @oav({ tooltip: "随着时间的推移，新粒子产生的速度。" })
         rateOverTime = serialization.setValue(new MinMaxCurve(), { between0And1: true, constant: 10, constant1: 10 });
 
-        // /**
-        //  * 产生新粒子的速度，通过距离。
-        //  */
-        // // @oav({ tooltip: "The rate at which new particles are spawned, over distance." })
-        // @oav({ tooltip: "产生新粒子的速度，通过距离。" })
-        // rateOverDistance = serialization.setValue(new MinMaxCurve(), { between0And1: true, constant: 0, constant1: 1 });
+        /**
+         * Change the rate over time multiplier.
+         * This is more efficient than accessing the whole curve, if you only want to change the overall rate multiplier.
+         * 
+         * 改变率随时间的乘数。
+         * 如果您只想更改整体的速率乘数，那么这比访问整个曲线更有效。
+         */
+        get rateOverTimeMultiplier()
+        {
+            return this.rateOverTime.curveMultiplier;
+        }
+
+        set rateOverTimeMultiplier(v)
+        {
+            this.rateOverTime.curveMultiplier = v;
+        }
+
+        /**
+         * The rate at which new particles are spawned, over distance.
+         * 
+         * 产生新粒子的速度，通过距离。
+         * 
+         * @todo
+         */
+        // @oav({ tooltip: "The rate at which new particles are spawned, over distance." })
+        @oav({ tooltip: "产生新粒子的速度，通过距离。" })
+        rateOverDistance = serialization.setValue(new MinMaxCurve(), { between0And1: true, constant: 0, constant1: 1 });
+
+        /**
+         * Change the rate over distance multiplier.
+         * This is more efficient than accessing the whole curve, if you only want to change the overall rate multiplier.
+         * 
+         * 改变速率随距离变化的乘数。
+         * 如果您只想更改整体的速率乘数，那么这比访问整个曲线更有效。
+         */
+        get rateOverDistanceMultiplier()
+        {
+            return this.rateOverDistance.curveMultiplier;
+        }
+
+        set rateOverDistanceMultiplier(v)
+        {
+            this.rateOverDistance.curveMultiplier = v;
+        }
 
         /**
          * 爆发，在time时刻额外喷射particles粒子
@@ -28,6 +66,8 @@ namespace feng3d
         bursts: ParticleEmissionBurst[] = [];
 
         /**
+         * The current number of bursts.
+         * 
          * 当前的爆发次数。
          */
         get burstCount()
