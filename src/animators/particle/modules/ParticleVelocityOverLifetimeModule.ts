4f8084e5f409743fcde750d9b7e4912c4c126d13
feng <908087098@qq.com>
2019-11-13 17:27:40 +0800

    调整 ParticleVelocityOverLifetimeModule 接口

--- src/animators/particle/modules/ParticleVelocityOverLifetimeModule.ts
+++ src/animators/particle/modules/ParticleVelocityOverLifetimeModule.ts
@@ -10,6 +10,7 @@ namespace feng3d
     {
         /**
          * Curve to control particle speed based on lifetime.
+         * 
          * 基于寿命的粒子速度控制曲线。
          */
         @serialize
@@ -19,11 +20,102 @@ namespace feng3d
 
         /**
          * Specifies if the velocities are in local space (rotated with the transform) or world space.
+         * 
          * 指定速度是在局部空间(与变换一起旋转)还是在世界空间。
          */
         // @oav({ tooltip: "Specifies if the velocities are in local space (rotated with the transform) or world space.", component: "OAVEnum", componentParam: { enumClass: ParticleSystemSimulationSpace1 } })
         @oav({ tooltip: "指定速度是在局部空间(与变换一起旋转)还是在世界空间。", component: "OAVEnum", componentParam: { enumClass: ParticleSystemSimulationSpace1 } })
         space = ParticleSystemSimulationSpace1.Local;
+
+        /**
+         * Curve to control particle speed based on lifetime, on the X axis.
+         * 
+         * 曲线控制粒子速度基于寿命，在X轴上。
+         */
+        get x()
+        {
+            return this.velocity.xCurve;
+        }
+
+        set x(v)
+        {
+            this.velocity.xCurve = v;
+        }
+
+        /**
+         * X axis speed multiplier.
+         * 
+         * X轴速度倍增器。
+         */
+        get xMultiplier()
+        {
+            return this.x.curveMultiplier;
+        }
+
+        set xMultiplier(v)
+        {
+            this.x.curveMultiplier = v;
+        }
+
+        /**
+         * Curve to control particle speed based on lifetime, on the Y axis.
+         * 
+         * 曲线控制粒子速度基于寿命，在Y轴上。
+         */
+        get y()
+        {
+            return this.velocity.yCurve;
+        }
+
+        set y(v)
+        {
+            this.velocity.yCurve = v;
+        }
+
+        /**
+         * Y axis speed multiplier.
+         * 
+         * Y轴速度倍增器。
+         */
+        get yMultiplier()
+        {
+            return this.y.curveMultiplier;
+        }
+
+        set yMultiplier(v)
+        {
+            this.y.curveMultiplier = v;
+        }
+
+        /**
+         * Curve to control particle speed based on lifetime, on the Z axis.
+         * 
+         * 曲线控制粒子速度基于寿命，在Z轴上。
+         */
+        get z()
+        {
+            return this.velocity.zCurve;
+        }
+
+        set z(v)
+        {
+            this.velocity.zCurve = v;
+        }
+
+        /**
+         * Z axis speed multiplier.
+         * 
+         * Z轴速度倍增器。
+         */
+        get zMultiplier()
+        {
+            return this.z.curveMultiplier;
+        }
+
+        set zMultiplier(v)
+        {
+            this.z.curveMultiplier = v;
+        }
 
         /**
          * 初始化粒子状态
