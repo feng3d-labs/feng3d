d6cf92c7e7cb00695a5c734d3070f49de4529d92
feng <908087098@qq.com>
2019-11-17 08:56:24 +0800

    定义 ExtractFunctionKeys，选取T类型中值为函数的所有键

--- out/feng3d.d.ts
+++ out/feng3d.d.ts
@@ -76,31 +76,102 @@ declare namespace feng3d {
 }
 declare namespace feng3d {
     /**
-     * 让T中以及所有属性中的所有属性都是可选的
+     * 让T中以及所有键值中的所有键都是可选的
      */
-    type gPartial<T> = {
+    export type gPartial<T> = {
         [P in keyof T]?: gPartial<T[P]>;
     };
     /**
      * 任意函数
      */
-    type AnyFunction = (...args: any) => any;
+    export type AnyFunction = (...args: any) => any;
+    type ExcludeTypeTemp<T, KT, K> = K extends keyof T ? (T[K] extends KT ? never : K) : never;
     /**
+     * 获取T类型中除值为KT类型以外的所有键
      *
+     * ```
+     * class A
+     * {
+     *      a = 1;
+     *      f(){}
+     * }
+     *
+     * var a: ExcludeTypeKeys<A, number>; //var a:"f"
+     * var a1: ExcludeTypeKeys<A, AnyFunction>; //var a:"a"
+     *
+     * ```
      */
-    type ExcludeType<K, KT, T> = K extends keyof T ? (T[K] extends KT ? never : K) : never;
-    type OmitType<T, KT> = Pick<T, ExcludeType<keyof T, KT, T>>;
-    type Lazy<T> = T | (() => T);
-    type LazyObject<T> = {
+    export type ExcludeTypeKeys<T, KT> = ExcludeTypeTemp<T, KT, keyof T>;
+    /**
+     * 剔除T类型中值为KT类型的键
+     * ```
+     *     class A
+     *     {
+     *         a = 1;
+     *         f(){}
+     *     }
+     *
+     *     var a: ExcludeType<A, number>; //var a: Pick<A, "f">
+     *     var a1: ExcludeType<A, AnyFunction>; //var a1: Pick<A, "a">
+     * ```
+     */
+    export type ExcludeType<T, KT> = Pick<T, ExcludeTypeKeys<T, KT>>;
+    type ExtractTypeTemp<T, KT, K> = K extends keyof T ? (T[K] extends KT ? K : never) : never;
+    /**
+     * 选取T类型中值为KT类型的所有键
+     *
+     * ```
+     *     class A
+     *     {
+     *         a = 1;
+     *         f(){}
+     *     }
+     *
+     *     var a: ExtractTypeKeys<A, number>; //var a: "a"
+     *     var a1: ExtractTypeKeys<A, AnyFunction>; //var a1: "f"
+     * ```
+     */
+    export type ExtractTypeKeys<T, KT> = ExtractTypeTemp<T, KT, keyof T>;
+    /**
+     * 选取T类型中值为函数的所有键
+     *
+     * ```
+     *     class A
+     *     {
+     *         a = 1;
+     *         f(){}
+     *     }
+     *
+     *     var a: ExtractFunctionKeys<A>; //var a: "f"
+     * ```
+     */
+    export type ExtractFunctionKeys<T> = ExtractTypeTemp<T, AnyFunction, keyof T>;
+    /**
+     * 选取T类型中值为KT类型的键
+     *
+     * ```
+     *     class A
+     *     {
+     *         a = 1;
+     *         f() { }
+     *     }
+     *
+     *     var a: ExtractType<A, number>; //var a: Pick<A, "a">
+     *     var a1: ExtractType<A, AnyFunction>; //var a1: Pick<A, "f">
+     * ```
+     */
+    export type ExtractType<T, KT> = Pick<T, ExtractTypeKeys<T, KT>>;
+    export type Lazy<T> = T | (() => T);
+    export type LazyObject<T> = {
         [P in keyof T]: Lazy<T[P]>;
     };
-    var lazy: {
+    export var lazy: {
         getvalue: <T>(lazyItem: Lazy<T>) => T;
     };
     /**
      * 可销毁对象
      */
-    interface IDisposable {
+    export interface IDisposable {
         /**
          * 是否已销毁
          */
@@ -110,6 +181,7 @@ declare namespace feng3d {
          */
         dispose(): void;
     }
+    export {};
 }
 /**
  * Object.assignDeep 中 转换结果的函数定义
--- src/polyfill/Types.ts
+++ src/polyfill/Types.ts
@@ -1,7 +1,7 @@
 namespace feng3d
 {
     /**
-     * 让T中以及所有属性中的所有属性都是可选的
+     * 让T中以及所有键值中的所有键都是可选的
      */
     export type gPartial<T> = {
         [P in keyof T]?: gPartial<T[P]>;
@@ -12,11 +12,87 @@ namespace feng3d
      */
     export type AnyFunction = (...args: any) => any;
 
+    type ExcludeTypeTemp<T, KT, K> = K extends keyof T ? (T[K] extends KT ? never : K) : never;
     /**
+     * 获取T类型中除值为KT类型以外的所有键
      * 
+     * ```
+     * class A
+     * {
+     *      a = 1;
+     *      f(){}
+     * }
+     * 
+     * var a: ExcludeTypeKeys<A, number>; //var a:"f"
+     * var a1: ExcludeTypeKeys<A, AnyFunction>; //var a:"a"
+     * 
+     * ```
      */
-    export type ExcludeType<K, KT, T> = K extends keyof T ? (T[K] extends KT ? never : K) : never;
-    export type OmitType<T, KT> = Pick<T, ExcludeType<keyof T, KT, T>>;
+    export type ExcludeTypeKeys<T, KT> = ExcludeTypeTemp<T, KT, keyof T>;
+    /**
+     * 剔除T类型中值为KT类型的键
+     * ```
+     *     class A
+     *     {
+     *         a = 1;
+     *         f(){}
+     *     }
+     * 
+     *     var a: ExcludeType<A, number>; //var a: Pick<A, "f">
+     *     var a1: ExcludeType<A, AnyFunction>; //var a1: Pick<A, "a">
+     * ```
+     */
+    export type ExcludeType<T, KT> = Pick<T, ExcludeTypeKeys<T, KT>>;
+
+    type ExtractTypeTemp<T, KT, K> = K extends keyof T ? (T[K] extends KT ? K : never) : never;
+    /**
+     * 选取T类型中值为KT类型的所有键
+     * 
+     * ```
+     *     class A
+     *     {
+     *         a = 1;
+     *         f(){}
+     *     }
+     * 
+     *     var a: ExtractTypeKeys<A, number>; //var a: "a"
+     *     var a1: ExtractTypeKeys<A, AnyFunction>; //var a1: "f"
+     * ```
+     */
+    export type ExtractTypeKeys<T, KT> = ExtractTypeTemp<T, KT, keyof T>;
+
+    /**
+     * 选取T类型中值为函数的所有键
+     * 
+     * ```
+     *     class A
+     *     {
+     *         a = 1;
+     *         f(){}
+     *     }
+     * 
+     *     var a: ExtractFunctionKeys<A>; //var a: "f"
+     * ```
+     */
+    export type ExtractFunctionKeys<T> = ExtractTypeTemp<T, AnyFunction, keyof T>;
+
+    /**
+     * 选取T类型中值为KT类型的键
+     * 
+     * ```
+     *     class A
+     *     {
+     *         a = 1;
+     *         f() { }
+     *     }
+     * 
+     *     var a: ExtractType<A, number>; //var a: Pick<A, "a">
+     *     var a1: ExtractType<A, AnyFunction>; //var a1: Pick<A, "f">
+     * ```
+     */
+    export type ExtractType<T, KT> = Pick<T, ExtractTypeKeys<T, KT>>;
+
+
 
     export type Lazy<T> = T | (() => T);
 
