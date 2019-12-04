9ddfe0852b85149eb560bb8cbea49e57516d54f4
feng <908087098@qq.com>
2019-12-04 13:52:59 +0800

    RectTransform

---
+++ src/ui/RectTransform.ts
@@ -0,0 +1,12 @@
+namespace feng3d
+{
+    /**
+     * Position, size, anchor and pivot information for a rectangle.
+     * 
+     * 矩形的位置、大小、锚点和枢轴信息。
+     */
+    export class RectTransform extends Transform
+    {
+
+    }
+}
