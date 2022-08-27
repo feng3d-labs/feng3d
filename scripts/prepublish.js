686f1f498f0f8a47d6157294d67c0ac1e49249ce
feng <908087098@qq.com>
2022-08-27 15:36:54 +0800

    "module": "src/index.ts",

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -4,6 +4,8 @@ const pkgpath = path.resolve('package.json');
 
 let pkg = fs.readFileSync(pkgpath, 'utf8');
 pkg = pkg.replace(`"main": "src/index.ts"`, `"main": "lib/index.js"`)
-    .replace(`"types": "src/index.ts"`, `"types": "lib/index.d.ts"`);
+    .replace(`"types": "src/index.ts"`, `"types": "lib/index.d.ts"`)
+    .replace(`"module": "src/index.ts"`, `"module": "lib/index.es.js"`)
+    ;
 
 fs.writeFileSync(pkgpath, pkg, 'utf8');
