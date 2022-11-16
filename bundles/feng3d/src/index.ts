e5c5c023f0aabc9aa24aa30a59b8f09f73d3c819
feng <908087098@qq.com>
2022-11-16 19:24:57 +0800

    feng3d

---
+++ bundles/feng3d/src/index.ts
@@ -0,0 +1,21 @@
+export * from '@feng3d/assets';
+export * from '@feng3d/bezier';
+export * from '@feng3d/core';
+export * from '@feng3d/event';
+export * from '@feng3d/filesystem';
+export * from '@feng3d/math';
+export * from '@feng3d/objectview';
+export * from '@feng3d/particlesystem';
+export * from '@feng3d/path';
+export * from '@feng3d/polyfill';
+export * from '@feng3d/renderer';
+export * from '@feng3d/serialization';
+export * from '@feng3d/shortcut';
+export * from '@feng3d/terrain';
+export * from '@feng3d/ui';
+export * from '@feng3d/watcher';
+
+/**
+ * 版本号
+ */
+export const version = '0.1.3';
