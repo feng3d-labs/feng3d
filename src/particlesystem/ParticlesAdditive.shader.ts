45acf666d6019e6904babc623e09f6e78b9c3e82
feng <908087098@qq.com>
2023-01-10 09:59:41 +0800

    RegisterParticleModule

--- src/particlesystem/ParticlesAdditive.shader.ts
+++ src/particlesystem/ParticlesAdditive.shader.ts
@@ -1,4 +1,4 @@
-import { Material } from '../core/materials/Material';
+import { Material, RegisterMaterial } from '../core/materials/Material';
 import { Texture2D } from '../core/textures/Texture2D';
 import { Color4 } from '../math/Color4';
 import { Vector4 } from '../math/geom/Vector4';
@@ -9,13 +9,15 @@ import { SerializeProperty } from '../serialization/SerializeProperty';
 
 declare module '../core/materials/Material'
 {
+    interface MaterialMap { ParticleMaterial: ParticleMaterial }
+    interface UniformsMap { ParticlesAdditiveUniforms: ParticlesAdditiveUniforms }
     interface DefaultMaterialMap
     {
         'Particle-Material': Material;
     }
 }
 
-@Serializable('ParticleMaterial')
+@RegisterMaterial('ParticleMaterial')
 export class ParticleMaterial extends Material
 {
     uniforms = new ParticlesAdditiveUniforms();
