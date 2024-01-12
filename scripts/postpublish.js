83f5a94de5e89cd3d888369cf694d087d36d7c70
feng <908087098@qq.com>
2024-01-12 21:09:11 +0800

    1

--- scripts/postpublish.js
+++ scripts/postpublish.js
@@ -1,12 +1,17 @@
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
+
 const pkgpath = path.resolve('package.json');
 
 let pkg = fs.readFileSync(pkgpath, 'utf8');
 pkg = pkg
-    .replace(`"main": "dist/index.umd.js"`, `"main": "src/index.ts"`)
-    .replace(`"types": "lib/index.d.ts"`, `"types": "src/index.ts"`)
-    .replace(`"module": "dist/index.mjs"`, `"module": "src/index.ts"`)
+    .replace(`"types": "./lib/index.d.ts"`, `"types": "./src/index.ts"`)
+    //
+    .replace(`"module": "./dist/index.js"`, `"module": "./src/index.ts"`)
+    .replace(`"main": "./dist/index.umd.cjs"`, `"main": "./src/index.ts"`)
+    //
+    .replace(`"import": "./dist/index.js"`, `"import": "./src/index.ts"`)
+    .replace(`"require": "./dist/index.umd.cjs"`, `"require": "./src/index.ts"`)
     ;
 
 fs.writeFileSync(pkgpath, pkg, 'utf8');
--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,11 +1,17 @@
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
+
 const pkgpath = path.resolve('package.json');
 
 let pkg = fs.readFileSync(pkgpath, 'utf8');
-pkg = pkg.replace(`"main": "src/index.ts"`, `"main": "dist/index.umd.js"`)
-    .replace(`"types": "src/index.ts"`, `"types": "lib/index.d.ts"`)
-    .replace(`"module": "src/index.ts"`, `"module": "dist/index.mjs"`)
+pkg = pkg
+    .replace(`"types": "./src/index.ts"`, `"types": "./lib/index.d.ts"`)
+    //
+    .replace(`"module": "./src/index.ts"`, `"module": "./dist/index.js"`)
+    .replace(`"main": "./src/index.ts"`, `"main": "./dist/index.umd.cjs"`)
+    //
+    .replace(`"import": "./src/index.ts"`, `"import": "./dist/index.js"`)
+    .replace(`"require": "./src/index.ts"`, `"require": "./dist/index.umd.cjs"`)
     ;
 
 fs.writeFileSync(pkgpath, pkg, 'utf8');
