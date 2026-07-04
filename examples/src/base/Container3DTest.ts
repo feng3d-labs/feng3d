4668e9df030cf73b029f5b37030a5fdd54cdc912
feng <908087098@qq.com>
2026-07-04 20:44:32 +0800

    fix: Container3DTest 在 new Object3D() 后调用 object3DLogic 触发 entityLogic
    确保 components.push 前注册组件自动初始化 effect。

--- examples/src/base/Container3DTest.ts
+++ examples/src/base/Container3DTest.ts
@@ -1,10 +1,15 @@
-import { Camera, Color4, ColorMaterial, Object3D, reactive, Renderable, Scene, ticker, View, object3DLogic, createPrimitive } from 'feng3d';
-const sceneObject3D = new Object3D(); reactive(sceneObject3D).name = "Untitled";
+import { Camera, Color4, ColorMaterial, createPrimitive, Object3D, object3DLogic, reactive, Renderable, Scene, ticker, View } from 'feng3d';
+const sceneObject3D = new Object3D();
+object3DLogic(sceneObject3D);
+reactive(sceneObject3D).name = "Untitled";
 const scene = new Scene(); reactive(sceneObject3D).components.push(scene);
 scene.background = new Color4(0.408, 0.38, 0.357, 1.0);
 
-const cameraObject3D = new Object3D(); reactive(cameraObject3D).name = "Main Camera";
-const camera = new Camera(); reactive(cameraObject3D).components.push(camera);
+const cameraObject3D = new Object3D();
+object3DLogic(cameraObject3D);
+reactive(cameraObject3D).name = "Main Camera";
+const camera = new Camera();
+reactive(cameraObject3D).components.push(camera);
 {
     const _r_pos = reactive(camera.object3D.position);
     _r_pos.x = 0; _r_pos.y = 1; _r_pos.z = -10;
@@ -24,13 +29,15 @@ reactive(cylinder.position).x = 2;
 object3DLogic(cube).addChild(cylinder);
 
 let num = 0;
-ticker.onframe(() => {
+ticker.onframe(() =>
+{
     //变化旋转与颜色
     reactive(cube.rotation).y += 1;
 
     num++;
 
-    if (num % 60 == 0) {
+    if (num % 60 == 0)
+    {
         reactive(colorMaterial.uniforms).u_diffuseInput = new Color4().fromUnit(Math.random() * (1 << 32 - 1));
     }
 });
