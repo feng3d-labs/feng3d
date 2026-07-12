7213900f9d2d069fa2ddf7fc799a51b9b76948a2
feng <908087098@qq.com>
2026-07-12 23:37:27 +0800

    1

--- examples/src/files.ts
+++ examples/src/files.ts
@@ -20,9 +20,6 @@ export default {
     "lights": [
         "PointLightTest",
     ],
-    "animator": [
-        "SceneLoadTest",
-    ],
     "advanced": [
         "TerrainTest",
         "TerrainMergeTest",
